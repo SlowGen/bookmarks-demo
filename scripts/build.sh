7e3db5b830d8e9ba55af4d44ff131fdb9cbf2af7
Kali <kali@kasheesh.co>
2024-08-26 16:43:41 -0400

    custom bootstrap!

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,4 +7,5 @@ tsc typescript/*.ts --outDir web --strict --target ES2021
 node ./scripts/pubspec-manifest.js
 
 # build flutter web
-flutter build web --web-renderer html --csp --pwa-strategy none
+# flutter build web --web-renderer html --csp --pwa-strategy none
+flutter build web --csp --no-web-resources-cdn --pwa-strategy none
