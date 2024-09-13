de9da2f9014f6c5e9fec59d6aeae43506fa0bf55
Kali <kali@kasheesh.co>
2024-09-13 13:54:05 -0400

    updated to utilities

--- lib/browser_extension/browser_interops/tabs_methods.dart
+++ lib/browser_extension/browser_interops/tabs_methods.dart
@@ -2,11 +2,11 @@ import 'dart:js_interop';
 
 import 'package:bookmarks/browser_extension/browser_interops/chrome_api.dart';
 import 'package:bookmarks/browser_extension/chrome_types.dart';
-import 'package:bookmarks/browser_extension/browser_interops/interop_extensions.dart';
+import 'package:bookmarks/browser_extension/browser_interops/utilities.dart';
 
 abstract class TabsMethods {
   static Future<Tab> create(String url, [bool active = true]) async {
-    final createProperties =
+    final JSObject createProperties =
         {'url': url.toJS, 'active': active.toJS}.toJSObject();
     return chrome.tabs.create(createProperties).toTab();
   }
